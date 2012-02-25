e6e9bd6928aba3dca1ed79df993217a80217b348
Jorge Zapata <jorgeluis.zapata@gmail.com>
2012-02-25 10:45:03 +0000

    Remove the context clip path

--- esvg/lib/parser/esvg_parser_clip_path.c
+++ esvg/lib/parser/esvg_parser_clip_path.c
@@ -41,10 +41,44 @@ static const char * _parser_clip_path_name_get(Edom_Tag *tag)
 	return "clipPath";
 }
 
+static Eina_Bool _parser_clip_path_child_supported(Edom_Tag *tag, int tag_id)
+{
+	switch (tag_id)
+	{
+		case ESVG_CIRCLE:
+		case ESVG_ELLIPSE:
+		case ESVG_RECT:
+		case ESVG_LINE:
+		case ESVG_PATH:
+		case ESVG_POLYLINE:
+		case ESVG_POLYGON:
+		case ESVG_TEXT:
+		case ESVG_USE:
+		return EINA_TRUE;
+
+		default:
+		return EINA_FALSE;
+	}
+}
+
+static Eina_Bool _parser_clip_path_child_add(Edom_Tag *tag, Edom_Tag *child)
+{
+	Enesim_Renderer *r;
+	Enesim_Renderer *rr = NULL;
+
+	r = esvg_parser_element_data_get(tag);
+	rr = esvg_parser_element_data_get(child);
+	if (r && rr)
+		esvg_container_element_add(r, rr);
+}
+
 static Edom_Tag_Descriptor _descriptor = {
 	/* .name_get 		= */ _parser_clip_path_name_get,
 	/* .attribute_set 	= */ _parser_clip_path_attribute_set,
 	/* .attribute_get 	= */ _parser_clip_path_attribute_get,
+	/* .child_supported	= */ _parser_clip_path_child_supported,
+	/* .child_add		= */ _parser_clip_path_child_add,
+	/* .child_remove	= */ NULL,
 };
 /*============================================================================*
  *                                 Global                                     *
