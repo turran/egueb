cd9018cf7a7ba7baf8bfc4cda8f3b32b3dd05a12
Jorge Zapata <jorgeluis.zapata@gmail.com>
2012-04-29 23:01:28 +0000

    Start creating the context. The data that will be passed on the setup, to be able to queue setup operations when not every reference is solutionable yet

---
+++ esvg/include/esvg_private_context.h
@@ -0,0 +1,16 @@
+#ifndef _ESVG_PRIVATE_CONTEXT_H_
+#define _ESVG_PRIVATE_CONTEXT_H_
+
+typedef struct _Esvg_Context Esvg_Context;
+
+struct _Esvg_Context
+{
+	int run;
+	Eina_List *queue;
+};
+
+void esvg_context_init(Esvg_Context *thiz);
+void esvg_context_setup_enqueue(Esvg_Context *thiz);
+void esvg_context_setup_dequeue(Esvg_Context *thiz);
+
+#endif
---
+++ esvg/lib/esvg_context.c
@@ -0,0 +1,55 @@
+/* Esvg - SVG
+ * Copyright (C) 2011 Jorge Luis Zapata, Vincent Torri
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library.
+ * If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include "esvg_private_main.h"
+#include "esvg_private_context.h"
+/*============================================================================*
+ *                                  Local                                     *
+ *============================================================================*/
+static int _run = 0;
+
+typedef struct _Esvg_Context_Setup
+{
+
+
+} Esvg_Context_Setup;
+/*============================================================================*
+ *                                 Global                                     *
+ *============================================================================*/
+void esvg_context_init(Esvg_Context *thiz)
+{
+	thiz->run = _run++;
+	thiz->queue = NULL;
+}
+
+void esvg_context_setup_enqueue(Esvg_Context *thiz)
+{
+
+}
+
+void esvg_context_setup_dequeue(Esvg_Context *thiz)
+{
+
+}
+/*============================================================================*
+ *                                   API                                      *
+ *============================================================================*/
+
