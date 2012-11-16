722bdad5d20f50252d18c644d4a8f647c766b032
Jorge Zapata <jorgeluis.zapata@gmail.com>
2012-11-16 16:14:10 +0000

    More v8 code

--- esvg/lib/esvg_scriptor_js_v8.cc
+++ esvg/lib/esvg_scriptor_js_v8.cc
@@ -20,6 +20,8 @@
 #include "esvg_private_svg.h"
 
 #include <v8.h>
+#include <map>
+
 using namespace v8;
 /*
  * We need to implement functions on ender, that way we can introspect
@@ -43,33 +45,104 @@ typedef struct _Esvg_Scriptor_Js_V8
 	Persistent<Context> context;
 } Esvg_Scriptor_Js_V8;
 
+/* FIXME or we either use the stl or an eina hash, but looks like
+ * we cannot access the pointer of the persistent handle to pass
+ * it to eina_hash?
+ */
+static std::map<const char *, Persistent<FunctionTemplate> > _prototypes;
+
+#if 0
+static Handle<Value> GetPointX(Local<String> property, const AccessorInfo &info)
+{
+	Local<Object> self = info.Holder();
+	Local<External> wrap = Local<External>::Cast(self->GetInternalField(0));
+	void* ptr = wrap->Value();
+	int value = static_cast<Point*>(ptr)->x_;
+	return Integer::New(value);
+}
+
+static void SetPointX(Local<String> property, Local<Value> value,
+		const AccessorInfo& info)
+{
+	Local<Object> self = info.Holder();
+	Local<External> wrap = Local<External>::Cast(self->GetInternalField(0));
+	void* ptr = wrap->Value();
+	static_cast<Point*>(ptr)->x_ = value->Int32Value();
+}
+#endif
+
 static void _v8_element_property_to_js(Ender_Property *prop,
 		void *data)
 {
+	Ender_Container *c;
+	const char *name = (const char *)data;
 
+	/* FIXME we need to figure out how to pass the
+	 * handle as a void *
+	 */
+	Persistent<FunctionTemplate> tmpl = _prototypes[name];
+	/* get the container of the property */
+	c = ender_property_container_get(prop);
+	/* for simple containers it is simple, for
+	 * compound containers, we need to use the specification
+	 * as found on the svg spec
+	 */
+	printf("property of name = %s\n", ender_property_name_get(prop));
+	/* add an accessor to the template */
 }
 
-static void _v8_element_descriptor_to_js(Esvg_Scriptor_Js_V8 *thiz,
+static Persistent<FunctionTemplate> _v8_element_descriptor_to_js(
+		Esvg_Scriptor_Js_V8 *thiz,
 		Ender_Descriptor *descriptor)
 {
+	const char *name;
+	const void *ot;
+
+	/* find the descriptor name in the hash of templates */
+	name = ender_descriptor_name_get(descriptor);
+	Persistent<FunctionTemplate> tmpl = _prototypes[name];
+
+	if (tmpl.IsEmpty())
+	{
+		/* create a new prototype and store it */
+		HandleScope handle_scope;
+		Handle<FunctionTemplate> l_tmpl = FunctionTemplate::New();
+		Handle<ObjectTemplate> o_tmpl = l_tmpl->InstanceTemplate();
+
+		o_tmpl->SetInternalFieldCount(1);
+		tmpl = Persistent<FunctionTemplate>::New(l_tmpl);
+		_prototypes[name] = tmpl;
+	}
 	/* start adding the properties for such descriptor */
 	ender_descriptor_property_list(descriptor, _v8_element_property_to_js,
-			NULL);
+			&tmpl);
 	/* TODO add the functions */
+
 	/* get the parent descriptor and do the same */
 	descriptor = ender_descriptor_parent(descriptor);
-	if (!descriptor) return;
-
-	_v8_element_descriptor_to_js(thiz, descriptor);
+	if (descriptor)
+	{
+		Persistent<FunctionTemplate> ptmpl =
+				_v8_element_descriptor_to_js(thiz, descriptor);
+		/* inherit from its parent */
+		tmpl->Inherit(ptmpl);
+	}
+	return tmpl;
 }
 
-static void _v8_element_to_js(Esvg_Scriptor_Js_V8 *thiz, Ender_Element *e)
+static Handle<Object> _v8_element_to_js(Esvg_Scriptor_Js_V8 *thiz, Ender_Element *e)
 {
 	Ender_Descriptor *descriptor;
+	HandleScope handle_scope;
 
 	descriptor = ender_element_descriptor_get(e);
-	/* TODO in case the descriptor is already registered dont do nothing */
-	_v8_element_descriptor_to_js(thiz, descriptor);
+	Persistent<FunctionTemplate> tmpl = _v8_element_descriptor_to_js(thiz,
+			descriptor);
+
+	Handle<Function> f = tmpl->GetFunction();
+	Handle<Object> obj = f->NewInstance();
+	obj->SetInternalField(0, External::New(e));
+	return handle_scope.Close(obj);
 }
 
 v8::Handle<v8::Value> Alert(const v8::Arguments& args)
@@ -94,17 +167,21 @@ static void * _v8_context_new(Ender_Element *e)
 	Persistent<Context> context;
 
 	thiz = (Esvg_Scriptor_Js_V8 *)calloc(1, sizeof(Esvg_Scriptor_Js_V8));
+	thiz->svg = e;
+
 	/* create the global object */
 	Handle<ObjectTemplate> global = ObjectTemplate::New();
 	/* add default implementation of the alert message */
 	global->Set(String::New("alert"), FunctionTemplate::New(Alert, External::New(thiz)));
-
 	context = Context::New(NULL, global);
-	/* TODO register all the types that we own?
-	 * i.e the js idl?
-	 */
+	/* enter the context so all the following operation are done there */
+	context->Enter();
+
+	/* store the context */
 	thiz->context = context;
-	thiz->svg = e;
+
+	/* add the svg document */
+	context->Global()->Set(String::New("document"), _v8_element_to_js(thiz, e));
 
 	return thiz;
 }
@@ -122,7 +199,6 @@ static void _v8_run(void *ctx, const char *s)
 	Esvg_Scriptor_Js_V8 *thiz = (Esvg_Scriptor_Js_V8 *)ctx;
 	HandleScope handle_scope;
 
-	thiz->context->Enter();
 	/* create the source string */
 	Handle<String> source = String::New(s);
 	/* compile it */
