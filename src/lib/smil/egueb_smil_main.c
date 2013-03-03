578b2a53067904bbfed53687beee9077150cf8b4
Jorge Zapata <jorgeluis.zapata@gmail.com>
2013-03-03 20:08:51 +0000

    TODO++

---
+++ src/lib/smil/egueb_smil_main.c
@@ -0,0 +1,33 @@
+/* Egueb
+ * Copyright (C) 2011 - 2013 Jorge Luis Zapata
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library.
+ * If not, see <http://www.gnu.org/licenses/>.
+ */
+/*============================================================================*
+ *                                  Local                                     *
+ *============================================================================*/
+/*============================================================================*
+ *                                 Global                                     *
+ *============================================================================*/
+/*============================================================================*
+ *                                   API                                      *
+ *============================================================================*/
+/* the thing is that we dont know how to animate every value type, so we need
+ * to register some types before, liek what esvg does with its types
+ */
+EAPI void egueb_smil_type_register(void)
+{
+
+}
