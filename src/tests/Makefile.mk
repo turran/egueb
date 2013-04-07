eb9c65170fa5678adbcb1625e5e3e3cd04fbe4a3
Jorge Zapata <jorgeluis.zapata@gmail.com>
2013-04-07 17:46:27 +0000

    Move the css test into the tests directory Fix the Makefile to build the css test and the dom test

--- src/tests/Makefile.mk
+++ src/tests/Makefile.mk
@@ -1,18 +1,28 @@
-tests_CPPFLAGS = \
--I. \
--I$(top_srcdir)/src/lib \
--I$(top_srcdir)/src/lib/renderer \
--I$(top_srcdir)/src/lib/object \
+tests_dom_CPPFLAGS = \
+-I$(top_srcdir)/src/lib/dom \
 @EDOM_CFLAGS@
 
-tests_LDADD = \
+tests_dom_LDADD = \
 $(top_builddir)/src/lib/libenesim.la \
 @EDOM_LIBS@
 
-check_PROGRAMS = \
+tests_css_CPPFLAGS = \
+-I$(top_srcdir)/src/lib/css \
+@ECSS_CFLAGS@
+
+tests_css_LDADD = \
+$(top_builddir)/src/lib/css/libecss.la \
+@ECSS_LIBS@
+
+check_PROGRAMS += \
+src/tests/egueb_css_test01 \
 src/tests/egueb_dom_node01
 
 src_tests_egueb_dom_node01_SOURCES = src/tests/egueb_dom_node01.c
-src_tests_egueb_dom_node01_LDADD = $(tests_LDADD)
-src_tests_egueb_dom_node01_CPPFLAGS = $(tests_CPPFLAGS)
+src_tests_egueb_dom_node01_CPPFLAGS = $(tests_dom_CPPFLAGS)
+src_tests_egueb_dom_node01_LDADD = $(tests_dom_LDADD)
+
+src_tests_egueb_css_test01_SOURCES = src/tests/egueb_css_test01.c
+src_tests_egueb_css_test01_CPPFLAGS = $(tests_css_CPPFLAGS)
+src_tests_egueb_css_test01_LDADD = $(tests_css_LDADD)
 
