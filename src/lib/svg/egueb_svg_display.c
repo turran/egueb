ed538471ccb487fd8eb83e4f9fdd753af135b526
Jorge Zapata <jorgeluis.zapata@gmail.com>
2014-01-17 20:14:20 +0100

    Make it compile

--- src/lib/svg/egueb_svg_display.c
+++ src/lib/svg/egueb_svg_display.c
@@ -27,39 +27,39 @@
 EAPI Eina_Bool egueb_svg_display_string_from(Egueb_Svg_Display *d, const char *attr)
 {
 	if (!strcmp(attr, "inline"))
-		*d = ESVG_DISPLAY_INLINE;
+		*d = EGUEB_SVG_DISPLAY_INLINE;
 	else if (!strcmp(attr, "block"))
-		*d = ESVG_DISPLAY_BLOCK;
+		*d = EGUEB_SVG_DISPLAY_BLOCK;
 	else if (!strcmp(attr, "list-item"))
-		*d = ESVG_DISPLAY_LIST_ITEM;
+		*d = EGUEB_SVG_DISPLAY_LIST_ITEM;
 	else if (!strcmp(attr, "run-in"))
-		*d = ESVG_DISPLAY_RUN_IN;
+		*d = EGUEB_SVG_DISPLAY_RUN_IN;
 	else if (!strcmp(attr, "compact"))
-		*d = ESVG_DISPLAY_COMPACT;
+		*d = EGUEB_SVG_DISPLAY_COMPACT;
 	else if (!strcmp(attr, "marker"))
-		*d = ESVG_DISPLAY_MARKER;
+		*d = EGUEB_SVG_DISPLAY_MARKER;
 	else if (!strcmp(attr, "table"))
-		*d = ESVG_DISPLAY_TABLE;
+		*d = EGUEB_SVG_DISPLAY_TABLE;
 	else if (!strcmp(attr, "inline-table"))
-		*d = ESVG_DISPLAY_INLINE_TABLE;
+		*d = EGUEB_SVG_DISPLAY_INLINE_TABLE;
 	else if (!strcmp(attr, "table-row-group"))
-		*d = ESVG_DISPLAY_TABLE_ROW_GROUP;
+		*d = EGUEB_SVG_DISPLAY_TABLE_ROW_GROUP;
 	else if (!strcmp(attr, "table-header-group"))
-		*d = ESVG_DISPLAY_TABLE_HEADER_GROUP;
+		*d = EGUEB_SVG_DISPLAY_TABLE_HEADER_GROUP;
 	else if (!strcmp(attr, "table-footer-group"))
-		*d = ESVG_DISPLAY_TABLE_FOOTER_GROUP;
+		*d = EGUEB_SVG_DISPLAY_TABLE_FOOTER_GROUP;
 	else if (!strcmp(attr, "table-row"))
-		*d = ESVG_DISPLAY_TABLE_ROW;
+		*d = EGUEB_SVG_DISPLAY_TABLE_ROW;
 	else if (!strcmp(attr, "table-column-group"))
-		*d = ESVG_DISPLAY_TABLE_COLUMN_GROUP;
+		*d = EGUEB_SVG_DISPLAY_TABLE_COLUMN_GROUP;
 	else if (!strcmp(attr, "table-column"))
-		*d = ESVG_DISPLAY_TABLE_COLUMN;
+		*d = EGUEB_SVG_DISPLAY_TABLE_COLUMN;
 	else if (!strcmp(attr, "table-cell"))
-		*d = ESVG_DISPLAY_TABLE_CELL;
+		*d = EGUEB_SVG_DISPLAY_TABLE_CELL;
 	else if (!strcmp(attr, "table-caption"))
-		*d = ESVG_DISPLAY_TABLE_CAPTION;
+		*d = EGUEB_SVG_DISPLAY_TABLE_CAPTION;
 	else if (!strcmp(attr, "none"))
-		*d = ESVG_DISPLAY_NONE;
+		*d = EGUEB_SVG_DISPLAY_NONE;
 	else
 		return EINA_FALSE;
 	return EINA_TRUE;
