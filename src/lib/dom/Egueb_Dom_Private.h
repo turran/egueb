7b5b5d10206d8d492ca5823639a2828d43d85536
Jorge Zapata <jorgeluis.zapata@gmail.com>
2014-01-28 00:59:35 +0100

    Add a private header for user-provided dom libs

---
+++ src/lib/dom/Egueb_Dom_Private.h
@@ -0,0 +1,30 @@
+/* Egueb_Dom - DOM
+ * Copyright (C) 2011 - 2013 Jorge Luis Zapata
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library.
+ * If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef _EGUEB_DOM_PRIVATE_H
+#define _EGUEB_DOM_PRIVATE_H
+
+/* This file is subject of ABI/API change, use at your own risk
+ * It is needed in case a lib wants to inherit from the different
+ * DOM interfaces
+ */
+
+#include "private/egueb_dom_node_private.h" 
+#include "private/egueb_dom_element_private.h" 
+#include "private/egueb_dom_document_private.h" 
+
+#endif
