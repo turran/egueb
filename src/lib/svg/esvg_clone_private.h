6bb1bffc453a1e660c2966e8a8cbacea386549b7
Jorge Zapata <jorgeluis.zapata@gmail.com>
2014-03-02 23:26:00 +0100

    Remove more deprecated files

--- src/lib/svg/esvg_clone_private.h
+++
@@ -1,8 +0,0 @@
-#ifndef _ESVG_PRIVATE_CLONE_H_
-#define _ESVG_PRIVATE_CLONE_H_
-
-void egueb_svg_clone_init(void);
-void egueb_svg_clone_shutdown(void);
-Ender_Element * egueb_svg_clone_new(Ender_Element *e);
-
-#endif
