b375e1af81ed13cd3d823d94647752259eaa2788
Jorge Zapata <jorgeluis.zapata@gmail.com>
2014-07-29 20:55:00 +0200

    Add the double list attribute

---
+++ src/lib/dom/egueb_dom_attr_double_list.c
@@ -0,0 +1,55 @@
+/* Egueb
+ * Copyright (C) 2011 - 2013 Jorge Luis Zapata
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library.
+ * If not, see <http://www.gnu.org/licenses/>.
+ */
+#include "egueb_dom_private.h"
+
+#include "egueb_dom_string.h"
+#include "egueb_dom_main.h"
+#include "egueb_dom_attr.h"
+#include "egueb_dom_list.h"
+#include "egueb_dom_value.h"
+#include "egueb_dom_value_double.h"
+#include "egueb_dom_value_double_list.h"
+
+#include "egueb_dom_attr_private.h"
+#include "egueb_dom_attr_object_private.h"
+#include "egueb_dom_attr_double_list.h"
+/*============================================================================*
+ *                                  Local                                     *
+ *============================================================================*/
+EGUEB_DOM_ATTR_OBJECT_BOILERPLATE(Egueb_Dom_Attr_String_List,
+		egueb_dom_value_double_list, egueb_dom_attr_double_list)
+/*============================================================================*
+ *                                 Global                                     *
+ *============================================================================*/
+/*============================================================================*
+ *                                   API                                      *
+ *============================================================================*/
+EAPI Egueb_Dom_Node * egueb_dom_attr_double_list_new(Egueb_Dom_String *name,
+		Egueb_Dom_List *def, Eina_Bool animatable, Eina_Bool stylable,
+		Eina_Bool inheritable)
+{
+	Egueb_Dom_Node *n;
+	
+	n = ENESIM_OBJECT_INSTANCE_NEW(egueb_dom_attr_double_list);
+	egueb_dom_attr_init(n, name, animatable, stylable, inheritable);
+	if (def)
+		egueb_dom_attr_set(n, EGUEB_DOM_ATTR_TYPE_DEFAULT, def);
+	return n;
+}
+
+
