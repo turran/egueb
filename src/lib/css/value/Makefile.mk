de68e22cca30a0f6c2de526a62d75ffd91ace047
Jorge Zapata <jorgeluis.zapata@gmail.com>
2014-10-06 21:14:07 +0200

    Port the font style

--- src/lib/css/value/Makefile.mk
+++ src/lib/css/value/Makefile.mk
@@ -2,11 +2,13 @@ dist_installed_cssheaders_DATA += \
 src/lib/css/value/egueb_css_value_color.h \
 src/lib/css/value/egueb_css_value_font_family.h \
 src/lib/css/value/egueb_css_value_font_size.h \
+src/lib/css/value/egueb_css_value_font_style.h \
 src/lib/css/value/egueb_css_value_font_weight.h
 
 src_lib_css_libegueb_css_la_SOURCES += \
 src/lib/css/value/egueb_css_value_color.c \
 src/lib/css/value/egueb_css_value_font_family.c \
 src/lib/css/value/egueb_css_value_font_size.c \
+src/lib/css/value/egueb_css_value_font_style.c \
 src/lib/css/value/egueb_css_value_font_weight.c
 
