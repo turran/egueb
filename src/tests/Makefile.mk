202021dff34f6c3940079869d3181cf781d43fba
Jorge Zapata <jorgeluis.zapata@gmail.com>
2015-05-12 20:31:56 +0200

    Better tests/check support

--- src/tests/Makefile.mk
+++ src/tests/Makefile.mk
@@ -1,3 +1,5 @@
+if ENS_ENABLE_TESTS
+
 noinst_LTLIBRARIES += src/tests/libmydom.la
 
 src_tests_libmydom_la_SOURCES = \
@@ -21,36 +23,56 @@ $(top_builddir)/src/lib/smil/libegueb_smil.la \
 
 tests_dom_CPPFLAGS = \
 $(src_lib_dom_libegueb_dom_la_IFLAGS) \
+@CHECK_CFLAGS@ \
 @EGUEB_DOM_CFLAGS@
 
 tests_dom_LDADD = \
 $(top_builddir)/src/lib/dom/libegueb_dom.la \
 $(top_builddir)/src/tests/libmydom.la \
+@CHECK_LIBS@ \
 @EGUEB_DOM_LIBS@
 
 tests_css_CPPFLAGS = \
--I$(top_srcdir)/src/lib/css \
+$(src_lib_css_libegueb_css_la_IFLAGS) \
 $(tests_dom_CPPFLAGS) \
+@CHECK_CFLAGS@ \
 @EGUEB_CSS_CFLAGS@
 
 tests_css_LDADD = \
 $(top_builddir)/src/lib/css/libegueb_css.la \
 $(tests_dom_LDADD) \
+@CHECK_LIBS@ \
 @EGUEB_CSS_LIBS@
 
+tests_smil_CPPFLAGS = \
+$(src_lib_smil_libegueb_smil_la_IFLAGS) \
+$(tests_dom_CPPFLAGS) \
+@CHECK_CFLAGS@ \
+@EGUEB_SMIL_CFLAGS@
+
+tests_smil_LDADD = \
+$(top_builddir)/src/lib/smil/libegueb_smil.la \
+$(tests_dom_LDADD) \
+@CHECK_LIBS@ \
+@EGUEB_SMIL_LIBS@
+
 tests_svg_CPPFLAGS = \
--I$(top_srcdir)/src/lib/svg \
+$(src_lib_svg_libegueb_svg_la_IFLAGS) \
 $(tests_dom_CPPFLAGS) \
 $(tests_css_CPPFLAGS) \
+$(tests_smil_CPPFLAGS) \
+@CHECK_CFLAGS@ \
 @EGUEB_SVG_CFLAGS@
 
 tests_svg_LDADD = \
 $(top_builddir)/src/lib/svg/libegueb_svg.la \
 $(tests_dom_LDADD) \
 $(tests_css_LDADD) \
+$(tests_smil_LDADD) \
+@CHECK_LIBS@ \
 @EGUEB_SVG_LIBS@
 
-check_PROGRAMS += \
+local_TESTS = \
 src/tests/egueb_dom_document01 \
 src/tests/egueb_dom_document02 \
 src/tests/egueb_dom_document03 \
@@ -59,7 +81,11 @@ src/tests/egueb_dom_element01 \
 src/tests/egueb_dom_element02 \
 src/tests/egueb_dom_node01 \
 src/tests/egueb_dom_node02 \
-src/tests/egueb_svg_test01
+src/tests/egueb_svg_test01 \
+src/tests/egueb_test_svg_animation01
+
+check_PROGRAMS += $(local_TESTS)
+TESTS += $(local_TESTS)
 
 src_tests_egueb_dom_node01_SOURCES = src/tests/egueb_dom_node01.c
 src_tests_egueb_dom_node01_CPPFLAGS = $(tests_dom_CPPFLAGS)
@@ -97,3 +123,8 @@ src_tests_egueb_svg_test01_SOURCES = src/tests/egueb_svg_test01.c
 src_tests_egueb_svg_test01_CPPFLAGS = $(tests_svg_CPPFLAGS)
 src_tests_egueb_svg_test01_LDADD = $(tests_svg_LDADD)
 
+src_tests_egueb_test_svg_animation01_SOURCES = src/tests/egueb_test_svg_animation01.c
+src_tests_egueb_test_svg_animation01_CPPFLAGS = $(tests_svg_CPPFLAGS)
+src_tests_egueb_test_svg_animation01_LDADD = $(tests_svg_LDADD)
+
+endif
