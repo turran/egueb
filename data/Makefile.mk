b50d32e814722f4a4567bac0c4d2036b73e64bb0
Jorge Zapata <jorgeluis.zapata@gmail.com>
2015-07-28 21:49:51 +0200

    Add the callbacks ender file

--- data/Makefile.mk
+++ data/Makefile.mk
@@ -15,6 +15,8 @@ endif
 
 ender: doxygen
 	@xsltproc --param lib "'egueb-dom'" --param version 0 --param case "'underscore'" @ENDER_TOOLS_DIR@/fromdoxygen.xslt $(top_builddir)/doc/xml/Egueb__Dom_8h.xml > out.xml
+	@xmllint -format out.xml > $(top_srcdir)/data/egueb-dom-main.ender
+	@xsltproc --param tomerge "'$(abs_top_srcdir)/data/egueb-dom-callbacks.ender'" @ENDER_TOOLS_DIR@/merge.xslt $(top_srcdir)/data/egueb-dom-main.ender > out.xml
 	@xmllint -format out.xml > $(top_srcdir)/data/egueb-dom.ender
 	@rm out.xml
 	@xsltproc --param lib "'egueb-svg'" --param version 0 --param case "'underscore'" @ENDER_TOOLS_DIR@/fromdoxygen.xslt $(top_builddir)/doc/xml/Egueb__Svg_8h.xml > out.xml
