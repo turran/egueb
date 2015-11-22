995841b30549e3d6b535eaf1c7c13d1ef329743d
Jorge Zapata <jorgeluis.zapata@gmail.com>
2015-11-22 11:37:26 +0100

    Remove redefines

--- src/lib/svg/Egueb_Svg.h
+++ src/lib/svg/Egueb_Svg.h
@@ -24,32 +24,6 @@
 #include <Egueb_Css.h>
 #include <Enesim.h>
 
-#ifdef EAPI
-# undef EAPI
-#endif
-
-#ifdef _WIN32
-# ifdef EGUEB_DOM_BUILD
-#  ifdef DLL_EXPORT
-#   define EAPI __declspec(dllexport)
-#  else
-#   define EAPI
-#  endif
-# else
-#  define EAPI __declspec(dllimport)
-# endif
-#else
-# ifdef __GNUC__
-#  if __GNUC__ >= 4
-#   define EAPI __attribute__ ((visibility("default")))
-#  else
-#   define EAPI
-#  endif
-# else
-#  define EAPI
-# endif
-#endif
-
 #ifdef __cplusplus
 extern "C" {
 #endif
