a24622232d68b6f5d02373478b06e41f75cd8745
Jorge Zapata <jorgeluis.zapata@gmail.com>
2015-12-02 22:17:25 +0100

    Add the possible implementation of a dict tree

--- src/lib/dom/egueb_dom_key.c
+++ src/lib/dom/egueb_dom_key.c
@@ -18,16 +18,75 @@
 #include "egueb_dom_private.h"
 
 #include "egueb_dom_string.h"
+/* TODO we should create a tree for easy matching if a key is or not printable
+ * like:
+ * input = Return
+ * first check if there is a non-printable string that starts with R, then
+ * if the next char can be e, then t, and so on ...
+ */
 /*============================================================================*
  *                                  Local                                     *
  *============================================================================*/
+typedef struct _Egueb_Dom_Key_Dict
+{
+	Eina_Inlist *roots;
+} Egueb_Dom_Key_Dict;
+
+typedef struct _Egueb_Dom_Key_Dict_Entry
+{
+	EINA_INLIST;
+	Eina_Unicode k;
+	Eina_Inlist *children;
+} Egueb_Dom_Key_Dict_Entry;
+
+
+static Eina_Bool _egueb_dom_key_dict_exists_recursive(Egueb_Dom_Key_Dict_Entry *entry,
+		const char *buf, int **index)
+{
+	return EINA_FALSE;
+}
+
+static Egueb_Dom_Key_Dict * _egueb_dom_key_dict_new(void)
+{
+	return NULL;
+}
+
+static void _egueb_dom_key_dict_free(Egueb_Dom_Key_Dict *thiz)
+{
+
+}
+
+
+static void _egueb_dom_key_dict_add(const char *str)
+{
+
+}
+
+static Eina_Bool _egueb_dom_key_dict_exists(Egueb_Dom_Key_Dict *thiz,
+		const char *str)
+{
+	/* iterate over the roots to find the first char */
+	/* if not found, return false */
+	/* if found, continue with the next entry */
+	return EINA_FALSE;
+}
 /*============================================================================*
  *                                 Global                                     *
  *============================================================================*/
+void egueb_dom_key_init(void)
+{
+	/* add every non-printable string */ 
+}
+
+void egueb_dom_key_shutdown(void)
+{
+
+}
 /*============================================================================*
  *                                   API                                      *
  *============================================================================*/
 EAPI Eina_Bool egueb_dom_key_is_printable(Egueb_Dom_String *s)
 {
+	/* check if the string exists on the non_printable tree */
 	return EINA_FALSE;
 }
